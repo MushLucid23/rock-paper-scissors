98fc822619456af497db25d2693aaf69f4b3f365
152053575+MushLucid23@users.noreply.github.com <152053575+MushLucid23@users.noreply.github.com>
2024-10-28 19:09:44 -0400

    Coded getHumanChoice in P,R or S
    Recoded getCompChoice, coded and tested getHumanChoice. Ready to move on.

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
-const choices = ["rock", "paper", "scissors"]
-
 function getComputerChoice() {
-    return choices[Math.floor(Math.random() * choices.length)]
+    const choices = ["rock", "paper", "scissors"];
+    return choices[Math.floor(Math.random() * 3)];
 }
+    console.log(getComputerChoice());
 
-console.log(getComputerChoice());
+function getHumanChoice() {
+    let choice = prompt("Choose Rock, Paper, or Scissors:").toLowerCase();
+        while (!["rock", "paper", "scissors"].includes(choice)) {
+            choice = prompt("Invalid choice. Please choose Rock, Paper, or Scissors:").toLowerCase();
+        }
+
+        return choice;
+}    
+
+let humanChoice = getHumanChoice();
+console.log("You chose:", humanChoice);
+
+    
+
+
  
     
 
